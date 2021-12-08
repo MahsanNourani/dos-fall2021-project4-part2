d79f6eb04d0615b6bd1c185dc22ae2f1f2f910fe
MahsanNourani <enchanted.mne@gmail.com>
2021-12-07 19:14:30 -0500

    overall template works and ready to code

--- Program.fs
+++ Program.fs
@@ -4,6 +4,13 @@ open Suave.Filters
 open Suave.Operators
 open Suave.Successful
 
+// open Suave.Http
+open Suave.Files
+// open Suave.RequestErrors
+// open Suave.Logging
+// open Suave.Utils
+// open Suave.WebSocket
+
 open System.Threading
 open Akka.Actor
 open Akka.FSharp
@@ -14,10 +21,12 @@ let app =
     let test = "gooz"
 
     choose [ GET
-             >=> choose [ path "/" >=> OK test
-                          path "/hello" >=> OK "Hello!" ]
+             >=> choose [ path "/" >=> file "index.html"
+                          path "/register"
+                          >=> OK "TODO: this should call register function!" ] // Mahsan
              POST
-             >=> choose [ path "/hello" >=> OK "Hello POST!" ] ]
+             >=> choose [ path "/login"
+                          >=> OK "TODO: this should call loging function!" ] ] // Shae
 
 [<EntryPoint>]
 let main argv =
